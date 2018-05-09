29396bead8b976c383c75ce42819b32cecc376f6
David Moffett <david.moffett@convergys.com>
2018-05-09 13:33:25 -0600

    Improved packagr process

--- public_api.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/app/ngst/ngst.module';
-export {EditorComponent} from './src/app/ngst/editor/editor.component';
-export * from './src/app/ngst/table/ngst-model';
---
+++ src/app/ngst/public_api.ts
@@ -0,0 +1,3 @@
+export * from './ngst.module';
+export {EditorComponent} from './editor/editor.component';
+export * from './table/ngst-model';
