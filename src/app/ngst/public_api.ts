f2ea5dbe65ebc92e7da77ad98608eb7884c01194
David Moffett <david.moffett@convergys.com>
2018-07-16 09:43:05 -0600

    Fix export

--- src/app/ngst/public_api.ts
+++ src/app/ngst/public_api.ts
@@ -1,5 +1,6 @@
 export * from './ngst.module';
 export {RawInputComponent} from './inputs/raw-input/raw-input.component';
+export {TextAreaInputComponent} from './inputs/raw-input/text-area-input.component';
 export {SelectionInputComponent} from './inputs/selection-input/selection-input.component';
 export {BooleanInputComponent} from './inputs/boolean-input/boolean-input.component';
 export * from './table/ngst-model';
