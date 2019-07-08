757b63c24ba8ff0c02d64b438d0b69add42548d0
Bruno González <bagonzalez.sv@gmail.com>
2019-07-08 16:44:35 -0600

    CXS-1030 tests fixed

--- src/app/ngst/dynamic-cell/dynamic-cell.component.spec.ts
+++ src/app/ngst/dynamic-cell/dynamic-cell.component.spec.ts
@@ -1,29 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DynamicCellComponent } from './dynamic-cell.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {ComponentFactoryResolver, NO_ERRORS_SCHEMA} from '@angular/core';
+import {PolymorphicContainerDirective} from "../directives/polymorphic-container.directive";
+
+import {DemoComponent} from "../../demo/demo.component";
+import {BrowserDynamicTestingModule} from "@angular/platform-browser-dynamic/testing";
+
 
 describe('DynamicCellComponent', () => {
   let component: DynamicCellComponent;
   let fixture: ComponentFixture<DynamicCellComponent>;
+  let componentFactoryResolver : ComponentFactoryResolver;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DynamicCellComponent],
-      imports: [HttpClientModule],
+      declarations: [ DynamicCellComponent, DemoComponent, PolymorphicContainerDirective],
       schemas : [NO_ERRORS_SCHEMA]
+    }).overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [DemoComponent]
+      }
     })
     .compileComponents();
+
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DynamicCellComponent);
     component = fixture.componentInstance;
+    component.component = DemoComponent;
+    componentFactoryResolver =  fixture.debugElement.injector.get(ComponentFactoryResolver);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+}); 
